30e4f5a9efb55ab8936b32990b093408e83fce45
KobayashiRiku <cccc12233@yahoo.co.jp>
2019-10-20 20:06:47 +0900

    Initial Commit

---
+++ test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  test
+//
+//  Created by kobayashi riku on 2019/10/20.
+//  Copyright © 2019 koba. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
